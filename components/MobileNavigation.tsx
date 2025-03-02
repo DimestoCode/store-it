e3f0af82c81db5514c1c8009494c8a3d6a10337a
dmytroandoniev <dmytro.andoniev@walkme.com>
2025-03-02 12:26:06 +0200

    Mobile navigation

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,7 +1,112 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
-const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import FileUploader from "./FileUploader";
+
+import type { IUser } from "@/types/user";
+import { cn } from "@/lib/utils";
+import { NAV_ITEMS } from "@/const";
+
+const MobileNavigation = ({ avatar, fullName, email }: IUser) => {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen p-3">
+          <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="avater"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+            </div>
+            <Separator className="mb-4 bg-light-200/20" />
+          </SheetTitle>
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {NAV_ITEMS.map(({ url, icon, name }) => {
+                return (
+                  <Link key={name} href={url} className="lg:w-full">
+                    <li
+                      className={cn(
+                        "mobile-nav-item",
+                        pathname === url && "shad-active"
+                      )}
+                    >
+                      <Image
+                        src={icon}
+                        alt={name}
+                        width={24}
+                        height={24}
+                        className={cn(
+                          "nav-icon",
+                          pathname === url && "nav-icon-active"
+                        )}
+                      />
+                      <p>{name}</p>
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+          </nav>
+          <Separator className="my-5 bg-light-200/20" />
+          <div className="flex flex-col justify-between gap-5 pb-5">
+            <FileUploader />
+            <Button
+              type="submit"
+              className="mobile-sign-out-button"
+              onClick={() => {}}
+            >
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="Logout"
+                width={24}
+                height={24}
+              />
+              <p>Logout</p>
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
 
 export default MobileNavigation;
