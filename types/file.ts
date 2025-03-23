b301a69f48735b824c1ad2c54a26b8b2e6311750
dmytroandoniev <dmytro.andoniev@walkme.com>
2025-03-23 09:25:06 +0200

    ui for file card

--- types/file.ts
+++ types/file.ts
@@ -1,10 +1,11 @@
 import { type Models } from "node-appwrite";
+import { IUser } from "./user";
 
 export interface IFile extends Models.Document {
   url: string;
   type: FileType;
   accountId: string;
-  owner: string;
+  owner: IUser;
   extension: string;
   size: number;
   users: string[];
